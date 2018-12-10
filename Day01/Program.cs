7edb76b914c41b0fb9b867643bcb2d64e0dd5d97
Dennis <dmanquestica@users.noreply.github.com>
2018-12-10 12:23:10 -0500

    AoC Day 1
    Day 1 Solution

---
+++ Day01/Program.cs
@@ -0,0 +1,59 @@
+﻿using SharedUtilities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventofCodeDay1Part2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var list = Utilities.ReadFile(args[0]);
+
+            var freqs = new List<int>();
+            foreach (var s in list)
+            {
+                freqs.Add(Int32.Parse(s));
+            }
+
+            Console.WriteLine("Part 1");
+
+            Console.WriteLine(freqs.Sum());
+
+            var currentFreq = 0;
+
+            Console.WriteLine("Part 2");
+
+            var existingFreqs = new HashSet<int>();
+
+            int iterations = 0;
+            for (int i = 0; i < freqs.Count(); ++i)
+            {
+                //Console.WriteLine(string.Format("Adding {0} to {1}", freqs[i], currentFreq));
+                currentFreq += freqs[i];
+                if (existingFreqs.Contains(currentFreq))
+                {
+                    Console.WriteLine(currentFreq);
+                    break;
+                }
+                else
+                {
+                    //Console.WriteLine(string.Format("Current Frequency: {0}", currentFreq));
+                    existingFreqs.Add(currentFreq);
+                }
+                if (i == freqs.Count - 1)
+                {
+                    //Console.WriteLine("Repeating list");
+                    i = -1;
+                }
+            }
+
+            Console.WriteLine("Iterations {0}", iterations);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SharedUtilities/Utilities.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedUtilities
+{
+    public class Utilities
+    {
+        public static IList<string> ReadFile(string path)
+        {
+            var list = new List<string>();
+            var file = path;
+
+            using (var fs = new FileStream(file, FileMode.Open, FileAccess.Read))
+            {
+                using (var sr = new StreamReader(fs))
+                {
+                    while (!sr.EndOfStream)
+                        list.Add(sr.ReadLine());
+                }
+            }
+
+            return list;
+        }
+    }
+}
